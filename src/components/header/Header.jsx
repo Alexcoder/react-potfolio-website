16b6650546a52b490c86826858cbf02d4b508d79
Alexcoder <agboalez@gmail.com>
2022-11-29 23:40:02 +0100

    background image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,14 +9,9 @@ const Header = () => {
        <div className="container header_container" id="home">
           <h5>Hello I'm</h5>
           <h1>Alex Agbo</h1>
-          {/* <h5 className='text-light'>Fullstack Developer</h5> */}
           <CTA/>
           <HeaderSocials/>
 
-          {/* <div className="me">
-             <img src={MAINPIC}  alt='me' className='now'/>
-          </div>
- */}
           <h1 className="description" >FULL STACK <br/> WEB DEVELOPER</h1>
           <a href='#contact' className='scroll_down'>Scroll Down</a>
        </div>
